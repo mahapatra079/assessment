780f79769ca8730e8ea462366632f3e56ca318ec
mahapatra079 <64601489+mahapatra079@users.noreply.github.com>
2023-11-18 12:11:23 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Map from "./components/Map.vue"
+import Polygon from "./components/Polygon.vue";
+</script>
+
+<template>
+ <div class="container">
+  <Map />
+  <Polygon />
+ </div>
+</template>
